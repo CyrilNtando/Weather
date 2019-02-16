b12b47f1bd1523a6020eea67609ff7b01c1b2458
ntando <ntandocyrilnkuna@gmail.com>
2019-02-16 13:39:11 +0200

    markup completed

---
+++ weather.js
@@ -0,0 +1,12 @@
+class Weather {
+  constructor(city, state) {
+    this.apiKey = "99dfe35fcb7de1ee";
+    this.city = city;
+    this.state = state;
+  }
+
+  //feach weather from API
+  async getWeather() {
+    const response = await fetch();
+  }
+}
